5e7f1d89356d95e08cf2e4c55a5e7ad8e4e6b945
filipmihal <fifomihal@gmail.com>
2022-08-21 17:18:35 -0700

    Async storage

---
+++ SmartCrawler/AsyncStorage.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata;
+
+namespace SmartCrawler;
+
+public class AsyncStorage<T>
+{
+    private readonly List<T> _list = new List<T>();
+
+    public void Add(T elem)
+    {
+        lock (_list)
+        {
+            _list.Add(elem);
+        }
+    }
+
+    public List<T> Export()
+    {
+        return _list;
+    }
+}
