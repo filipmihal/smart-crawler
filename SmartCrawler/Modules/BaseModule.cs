f9b94857884ff29f1cde6bab4f2cb2e10c98b004
Filip <fifomihal@gmail.com>
2022-09-27 09:09:49 -0700

    MOdules docs

--- SmartCrawler/Modules/BaseModule.cs
+++ SmartCrawler/Modules/BaseModule.cs
@@ -1,13 +1,28 @@
 namespace SmartCrawler.Modules;
 
-
+/**
+* Base interface for all modules
+* Each module must be inherited from this interface
+*/
 public interface IBaseModule
 {
-    
+    /// <summary>
+    ///  Matches a DatasetItem property to the result of the processing function
+    /// </summary>
+    /// <remarks>
+    /// Returns an action the matches a property instead of matching it right away. 
+    /// That is done because all actions are then ran together in the crawler.
+    /// </remarks>
     public Action<string, DatasetItem> Setup();
 }
 
 public interface IBaseModuleSetup<out T> : IBaseModule
 {
+    /// <summary>
+    ///  Processes the raw html content and returns the data in a form of a pre-set module data type
+    /// </summary>
+    /// <remarks>
+    /// Most of the processing logic is comprised of complex regex expressions
+    /// </remarks>
     public T Process(string html);
 }
--- SmartCrawler/Modules/DatasetItem.cs
+++ SmartCrawler/Modules/DatasetItem.cs
@@ -2,12 +2,28 @@ using SmartCrawler.Modules.Contacts;
 using SmartCrawler.Modules.CryptoWallets;
 
 namespace SmartCrawler.Modules;
-    
+
+/**
+* Represents the final data format that is going to be used after the crawling has finished
+* Each module must have its property defined here. Otherwise, the module dataset would not be accessible
+*  
+* Each module property must be marked as nullable because of the optional nature of every module
+*/  
 public class DatasetItem
 {
+
+    /// <summary>
+    /// URL is a required property that identifies each item in the final list
+    /// </summary>
     public string Url { get; set; }
+
+    // These two properties are null only if the CrawlingDepthOptions is not defined or its value is set to 0
     public string[]? SubUrls { get; set; }
     public int? Depth { get; set; }
+
+
+    // MODULE PROPERTIES
+    // Set them as nullable
     public ContactsDataset? Contacts { get; set; }
     public CryptoWalletsDataset? Wallets { get; set; }
 
