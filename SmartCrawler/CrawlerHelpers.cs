17ac1359021e0afc3eb833818057454b878e84a5
Filip <fifomihal@gmail.com>
2022-09-27 03:44:42 -0700

    crawler helpers docs

--- SmartCrawler/CrawlerHelpers.cs
+++ SmartCrawler/CrawlerHelpers.cs
@@ -4,6 +4,19 @@ using System.Text.RegularExpressions;
 
 public static class CrawlerHelpers
 {
+    /// <summary>
+    /// Filters cross-domain urls if <see cref="crossDomain"/> is set to true and formats the rest to a form of a <see cref="CrawledSite"/> object
+    /// </summary>
+    /// <param name="rawUrls">
+    /// Array of urls that needs to be filtered and formatted. Be aware that the urls in the array must be of the same crawling level. 
+    /// The will be given the same depth.
+    /// </param>
+    /// <param name="newDepth">New depth is the same for all urls</param>
+    /// <param name="crossDomain">Boolean that determines whether to </param>
+    /// <param name="parentUrl">Url according to which the filtering is performed</param>
+    /// <remarks>
+    /// subdomains are considered as a subset of the domain. Hence, they are not filtered.
+    /// </remarks>
     public static List<CrawledSite> FilterAndFormatUrls(List<string> rawUrls, int newDepth, bool crossDomain, string parentUrl)
     {
         List<CrawledSite> sitesToCrawl = new List<CrawledSite>();
@@ -19,12 +32,22 @@ public static class CrawlerHelpers
                     continue;
                 }
             }
+            //TODO: what about retries??
             sitesToCrawl.Add(new CrawledSite(url, newDepth));
         }
 
         return sitesToCrawl;
     }
 
+    /// <summary>
+    /// Parses links from a given string (Html)
+    /// Only valid links are parsed. Links that navigate to the current url are avoided
+    /// </summary>
+    /// <param name="inputString">raw html content</param>
+    /// <param name="parentUrl">Url from which the html content originates</param>
+    /// <remarks>
+    /// This function also processes relative links and appends the correct uri to the paths
+    /// </remarks>
     public static List<string> ParseLinks(string inputString, string parentUrl)
     {
         List<string> links = new List<string>();
@@ -33,7 +56,7 @@ public static class CrawlerHelpers
         {
             string link = match.Groups[2].ToString();
 
-            if (link.StartsWith("http"))
+            if (link.StartsWith("http") || link.StartsWith("www.") )
             {
                 links.Add(link);
             }
